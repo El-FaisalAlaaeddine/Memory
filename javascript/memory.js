a5811ed7593761df67b155b856a86a0305a0d863
El Faisal Alaaeddine <faisalalaaeddine2000@gmail.com>
2022-05-22 20:55:59 +0300

    incrementing level number

--- javascript/memory.js
+++ javascript/memory.js
@@ -66,3 +66,15 @@ function myFunc(id)
 			}
 	  
 
+function countMyself() {
+    // Check to see if the counter has been initialized
+	if ( typeof countMyself.counter == 'undefined' ) {
+        // It has not... perform the initialization
+        countMyself.counter = 0;
+    }
+	// Do something stupid to indicate the value
+     ++countMyself.counter;
+	 return countMyself.counter;
+	 
+}
+
