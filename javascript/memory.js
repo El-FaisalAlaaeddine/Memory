b4c3f5b0003528d870aeebba5c516f0a83c73be5
El Faisal Alaaeddine <faisalalaaeddine2000@gmail.com>
2022-05-22 20:54:22 +0300

    addaing start function

--- javascript/memory.js
+++ javascript/memory.js
@@ -6,3 +6,42 @@
 	document.addEventListener('keypress', start);
 
 	
+	
+	function start(){
+	var x = document.getElementById("level").innerHTML = "Level ";
+		var value = arr[Math.floor(Math.random() * arr.length)];
+
+		document.getElementById(value).style.backgroundColor = "white";
+			
+		function changeDiv(){
+			document.getElementById(value).style.backgroundColor = value;
+		}
+		
+		/*
+	if(value == 'red'){
+		var audio = document.getElementById("blueaudio");
+	}
+         
+      if(value == 'blue') {
+		var audio = document.getElementById("blueaudio");
+	  } 
+		
+	if(value == 'yellow') {
+		var audio = document.getElementById("yellowaudio");
+	}
+		
+	if(value == 'green'){
+		var audio = document.getElementById("greenaudio");
+	}
+		
+	audio.play();*/
+	playSound(value);
+	temp.push(value);
+	c = temp.length;
+	
+	var counter = countMyself();
+	document.getElementById("xxx").innerHTML = counter;
+	document.getElementById("demo").innerHTML = temp;
+}
+
+
